1946fb93b87f28f6b51a2e8310b447244c39d56e
mathis <mathis.contact@gmail.com>
2021-02-22 14:59:14 +0100

    - Added Nvidia Optimus support

--- src/ExGame/main.cpp
+++ src/ExGame/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <memory>
+#include <stddef.h>
 
 #include <EXP_Game.h>
+
+//Enabling Optimus
+extern "C" { _declspec(dllexport) uint32_t NvOptimusEnablement = 0x00000001; }
 
 int main(int argc, char* argv[]) {
 	std::unique_ptr<EXP_Game> game = std::make_unique<EXP_Game>("GameInfo.json");
