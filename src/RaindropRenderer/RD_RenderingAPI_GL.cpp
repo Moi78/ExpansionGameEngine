fe4bb3f860782e3e648d9aab2d6c8743582e57b5
mathis <mathis.contact@gmail.com>
2021-04-03 16:28:39 +0200

    - set ogl version to 4

--- src/RaindropRenderer/RD_RenderingAPI_GL.cpp
+++ src/RaindropRenderer/RD_RenderingAPI_GL.cpp
@@ -28,8 +28,8 @@ bool RD_WindowingSystemGLFW::OpenWindow(std::string name, int w, int h) {
 		return false;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_SAMPLES, 4);
