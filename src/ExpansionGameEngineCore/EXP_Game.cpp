87b8bf708f858c21f9cf16d703d71022f3f10470
Mathis <mathis.contact@gmail.com>
2022-09-18 13:54:13 +0200

    very small advancement

--- src/ExpansionGameEngineCore/EXP_Game.cpp
+++ src/ExpansionGameEngineCore/EXP_Game.cpp
@@ -1,5 +1,1 @@
-//
-// Created by mathis on 10/09/22.
-//
-
 #include "EXP_Game.h"
--- src/ExpansionGameEngineCore/EXP_Game.h
+++ src/ExpansionGameEngineCore/EXP_Game.h
@@ -1,8 +1,17 @@
 #ifndef EXPGE_EXP_GAME_H
 #define EXPGE_EXP_GAME_H
 
+#include <memory>
+#include <string>
+
+#include <RaindropRenderer.h>
 
 class EXP_Game {
+public:
+    EXP_Game(std::shared_ptr<RaindropRenderer> rndr);
+    ~EXP_Game();
+
+private:
 
 };
 
